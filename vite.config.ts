c08e6b28b7aca446309e6b88b2262ef50a1335f3
Movsar-Khalakhoev <movsar.kh.128@gmail.com>
2022-10-20 18:31:29 +0300

    в алиасы добавлена папка utils

--- vite.config.ts
+++ vite.config.ts
@@ -18,6 +18,7 @@ export default defineConfig({
       pages: path.resolve(__dirname, './src/pages'),
       store: path.resolve(__dirname, './src/store'),
       types: path.resolve(__dirname, './src/types'),
+      utils: path.resolve(__dirname, './src/utils'),
     },
   },
 })
