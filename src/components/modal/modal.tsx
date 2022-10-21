b335ab5b56cdc772ec7f102dceb732979ee3c858
merzhoev <arbimerzhoev@gmai.com>
2022-10-21 21:23:04 +0300

    feature-15: Реализовал компонент Modal

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -1,9 +1,20 @@
-import { PropsWithChildren } from 'react'
+import React, { PropsWithChildren } from 'react'
+import styles from './modal.module.scss'
 
 interface ModalProps extends PropsWithChildren {
   isShown: boolean
+  onClose: (isShown: boolean) => void
 }
 
-export function Modal(props: ModalProps) {
-  return <div />
+export function Modal({ isShown, onClose, children }: ModalProps) {
+  const handleOverlayClick = () => {
+    onClose(false)
+  }
+
+  return isShown ? (
+    <div className={styles.container}>
+      <div onClick={handleOverlayClick} className={styles.overlay} />
+      <div className={styles.modal}>{children}</div>
+    </div>
+  ) : null
 }
