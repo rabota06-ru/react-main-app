4f0125754d6d64ffd6bd53a2369d6f052cbd461c
Movsar-Khalakhoev <movsar.kh.128@gmail.com>
2022-10-21 19:46:47 +0300

    feature-18: добавил тег span

--- src/components/close-button/close-button.tsx
+++ src/components/close-button/close-button.tsx
@@ -3,5 +3,9 @@ import { ButtonHTMLAttributes } from 'react'
 interface CloseButtonProps extends Omit<Props<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>, 'type'> {}
 
 export function CloseButton(props: CloseButtonProps) {
-  return <button type='button' {...props} />
+  return (
+    <button type='button' {...props}>
+      <span />
+    </button>
+  )
 }
