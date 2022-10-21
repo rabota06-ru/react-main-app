82c8ca82f0207d9cc453866e5b8b589db6b76a09
Movsar-Khalakhoev <movsar.kh.128@gmail.com>
2022-10-21 21:21:09 +0300

    feature-18: добавлена небольшая jsdoc документация

--- src/components/close-button/close-button.tsx
+++ src/components/close-button/close-button.tsx
@@ -4,6 +4,9 @@ import styles from './close-button.module.scss'
 
 interface CloseButtonProps extends Omit<Props<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>, 'type'> {}
 
+/**
+ * Кнопка закрытия. Обычно используется в модалках
+ */
 export function CloseButton({ className, ...props }: CloseButtonProps) {
   return <button className={cn(styles.button, className)} type='button' {...props} />
 }
