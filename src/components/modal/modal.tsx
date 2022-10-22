4f5e813a0a3cfed7f4513db4c2148a1b039a2da5
merzhoev <arbimerzhoev@gmail.com>
2022-10-22 20:27:13 +0300

    feature-15: Добавлена jsdoc документация

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -1,11 +1,17 @@
-import React, { PropsWithChildren } from 'react'
+import { PropsWithChildren } from 'react'
 import styles from './modal.module.scss'
 
 interface ModalProps extends PropsWithChildren {
   isShown: boolean
   onClose: () => void
 }
-
+/**
+ * Модальное окно
+ * @param isShown флаг показа/скрытия модального окна
+ * @param onClose функция, которая меняет isShown на false
+ * @param children контент модального окна
+ * @example <Modal isShown={isShown} onClose={() => setIsShown(false)}>контент</Modal>
+ */
 export function Modal({ isShown, onClose, children }: ModalProps) {
   const handleOverlayClick = () => {
     onClose()
