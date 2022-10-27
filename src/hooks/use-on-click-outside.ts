3a5c4a55343057d6ab2a25769cba8a603c9a83c8
Movsar-Khalakhoev <movsar.kh.128@gmail.com>
2022-10-27 19:27:52 +0300

    feature-8: добавлена jsdoc документация на хук useOnClickOutside

--- src/hooks/use-on-click-outside.ts
+++ src/hooks/use-on-click-outside.ts
@@ -1,5 +1,10 @@
 import { useEffect, useRef } from 'react'
 
+/**
+ * Хук, который позволяет выполнять действия при клике снаружи элемента
+ * @param func функция, которая будет выполняться при клике снаружи
+ * @returns ref, который нужно установить на элемент, за границы которого клик должен отслеживаться
+ */
 export function useOnClickOutside<T extends HTMLElement>(func: () => any) {
   const ref = useRef<T>(null)
 
