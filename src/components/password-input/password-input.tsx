719ef06da55b152fd08727f493e0de4cff71c561
Malsagov <rus.malsagov177@mail.ru>
2022-10-31 12:10:57 +0300

    feature-14: Удалил стили. Добавил jsdoc документацию

--- src/components/password-input/password-input.tsx
+++ src/components/password-input/password-input.tsx
@@ -6,6 +6,16 @@ import styles from './password-input.module.scss'
 
 interface PasswordInputProps extends Omit<InputProps, 'type' | 'RightContent'> {}
 
+/**
+ * @param label текст, который отображается над инпутом
+ * @param status статус инпута, от которого зависит какого цвета обводка и нижний текст
+ * @param size размер инпута. Default - высота 50px, Large - высота 60px
+ * @param bottomText нижний текст
+ * @param isDisabled заблокирован ли инпут
+ * @param wrapperProps пропсы элемента, который оборачивает children
+ * @param isLoading флаг загрузки. Если true, то отображается спиннер и инуп заблокирован
+ */
+
 export function PasswordInput({ className, ...props }: PasswordInputProps) {
   const [isPasswordVisible, setIsPasswordVisible] = useState<boolean>(false)
   const HideOrShowPassword = isPasswordVisible ? AiOutlineEyeInvisible : AiOutlineEye
