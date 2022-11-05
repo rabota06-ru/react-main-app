96fceaffdf9e74071a10ea399f9efd5aab36b5e9
Movsar-Khalakhoev <movsar.kh.128@gmail.com>
2022-11-05 21:16:21 +0300

    feature-24: добавлен адаптив для секции "Найди свою работу"

--- src/pages/main-page/components/find-your-job-section/find-your-job-section.tsx
+++ src/pages/main-page/components/find-your-job-section/find-your-job-section.tsx
@@ -1,6 +1,7 @@
-import { Button } from 'components/button'
+import { Button, ButtonSize } from 'components/button'
 import { Container } from 'components/container'
 import cn from 'classnames'
+import { useMediaValue } from 'hooks/use-media-value'
 import styles from './find-your-job-section.module.scss'
 import firstImage from './first.png'
 import secondImage from './second.png'
@@ -9,19 +10,25 @@ import fourthImage from './fourth.png'
 import fifthImage from './fifth.png'
 
 export function FindYourJobSection() {
+  const buttonSize = useMediaValue({ xs: ButtonSize.Small, sm: ButtonSize.Medium })
+
   return (
-    <Container className={styles.section}>
-      <div className={styles.sectionContent}>
-        <h2 className={styles.sectionContentTitle}>
-          Найди работу <span className={styles.sectionContentTitleSelected}>прямо сейчас!</span>
-        </h2>
-        <Button isShadow>Вход и регистрация</Button>
-        <img className={cn(styles.sectionContentImage, styles.sectionContentImageFirst)} src={firstImage} alt='первая картинка' />
-        <img className={cn(styles.sectionContentImage, styles.sectionContentImageSecond)} src={secondImage} alt='вторая картинка' />
-        <img className={cn(styles.sectionContentImage, styles.sectionContentImageThird)} src={thirdImage} alt='третья картинка' />
-        <img className={cn(styles.sectionContentImage, styles.sectionContentImageFourth)} src={fourthImage} alt='четвёртая картинка' />
-        <img className={cn(styles.sectionContentImage, styles.sectionContentImageFifth)} src={fifthImage} alt='пятая картинка' />
-      </div>
-    </Container>
+    <div className={styles.section}>
+      <Container className={styles.sectionContainer}>
+        <div className={styles.sectionContent}>
+          <h2 className={styles.sectionContentTitle}>
+            Найди работу <span className={styles.sectionContentTitleSelected}>прямо сейчас!</span>
+          </h2>
+          <Button isShadow size={buttonSize} className={styles.sectionContentButton}>
+            Вход и регистрация
+          </Button>
+          <img className={cn(styles.sectionContentImage, styles.sectionContentImageFirst)} src={firstImage} alt='первая картинка' />
+          <img className={cn(styles.sectionContentImage, styles.sectionContentImageSecond)} src={secondImage} alt='вторая картинка' />
+          <img className={cn(styles.sectionContentImage, styles.sectionContentImageThird)} src={thirdImage} alt='третья картинка' />
+          <img className={cn(styles.sectionContentImage, styles.sectionContentImageFourth)} src={fourthImage} alt='четвёртая картинка' />
+          <img className={cn(styles.sectionContentImage, styles.sectionContentImageFifth)} src={fifthImage} alt='пятая картинка' />
+        </div>
+      </Container>
+    </div>
   )
 }
