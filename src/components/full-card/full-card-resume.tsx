403d237f66167a382928d8c6b40e09f2518838e3
Malsagov <rus.malsagov177@mail.ru>
2022-11-11 17:07:59 +0300

    feature-25: изменил флажок на svg

--- src/components/full-card/full-card-resume.tsx
+++ src/components/full-card/full-card-resume.tsx
@@ -6,8 +6,9 @@ import { HiOutlineEye } from 'react-icons/hi'
 import { Button, ButtonSize, ButtonVariant } from 'components/button'
 import { useState, createElement, useMemo } from 'react'
 import { Container } from 'components/container'
+import cn from 'classnames'
 import { FIELDS_OF_ACTIVITY_IMAGE } from 'utils/fields-of-activity'
-import favorites from './img/favorites.svg'
+// import favorites from './img/favorites.svg'
 // import favoritesTrue from './img/favoritesTrue.svg'
 import styles from './full-card-resume.module.scss'
 
@@ -47,7 +48,7 @@ export function FullCardResume({
   return (
     <Container>
       <Card className={styles.card} shadow={{ blurRadius: 40, spreadRadius: 5 }}>
-        <div className={styles.fullCard}>
+        <div {...props} className={styles.fullCard}>
           <div className={styles.fullCardHeader}>
             <div className={styles.fullCardHeaderIcon}>
               <div>{headerImageComponent}</div>
@@ -93,7 +94,21 @@ export function FullCardResume({
               {phoneState ? phoneNumber : 'Показать телефон'}
             </Button>
             <div>
-              <img src={favorites} alt='флажок' />
+              <svg
+                className={cn(styles.fullCardDescriptionButtonsCheckbox_NoShaded, {
+                  [styles.fullCardDescriptionButtonsCheckbox_Shaded]: isFavorite,
+                })}
+                width='40'
+                height='49'
+                viewBox='0 0 40 49'
+                fill='none'
+                xmlns='http://www.w3.org/2000/svg'
+              >
+                <path
+                  d='M0.5 2C0.5 1.17157 1.17157 0.5 2 0.5H38C38.8284 0.5 39.5 1.17157 39.5 2V46.3915C39.5 47.5697 38.2041 48.2879 37.205 47.6635L21.325 37.7385C20.5143 37.2318 19.4857 37.2318 18.675 37.7385L2.795 47.6635C1.79593 48.2879 0.5 47.5697 0.5 46.3915V2Z'
+                  stroke='#395DDE'
+                />
+              </svg>
             </div>
           </div>
         </div>
