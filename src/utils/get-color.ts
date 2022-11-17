e9c01813d1121539e5d0986a1b8e451c2dd2a258
merzhoev <arbimerzhoev@gmail.com>
2022-11-17 17:07:32 +0300

    feature-1: Сделан компонент Spinner

---
+++ src/utils/get-color.ts
@@ -0,0 +1,19 @@
+import { Color } from 'types';
+
+const colorVariableNames: Record<Color, string> = {
+  [Color.PrimaryColor]: '--primaryColor',
+  [Color.AttentionColor]: '--attentionColor',
+  [Color.SecondaryButtonColor]: '--secondaryButtonColor',
+  [Color.ButtonSecondaryTextColor]: '--buttonSecondaryTextColor',
+  [Color.ButtonTextColor]: '--buttonTextColor',
+};
+
+/**
+ * Возвращает строку для цвета в формате var(--cssVariable)
+ * @param color Цвет, для которого нужно получить строку с css-переменной
+ * @returns Строка с css-переменной
+ * @example var(--primaryColor)
+ */
+export function getColorVariable(color: Color) {
+  return `var(${colorVariableNames[color]})`;
+}
