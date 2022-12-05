c67d47e6bd132b17e237556c5a7b4c510f95f9b9
Movsar-Khalakhoev <movsar.kh.128@gmail.com>
2022-12-05 19:14:05 +0300

    Изменение в конфигурации vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
+import mkcert from 'vite-plugin-mkcert'
 import * as path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svgr(), react()],
+  server: {
+    host: '127.0.0.1',
+  },
+  plugins: [svgr(), mkcert(), react()],
   css: {
     modules: {
       localsConvention: 'camelCase',
