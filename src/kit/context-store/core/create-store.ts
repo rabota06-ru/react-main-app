e04e451fa1d130a4f26e6b6e668a715695301538
Movsar-Khalakhoev <movsar.kh.128@gmail.com>
2023-01-06 17:02:36 +0300

    feature-69: теперь функционал состояния полностью отделён от адаптера для реакта

--- src/kit/context-store/core/create-store.ts
+++ src/kit/context-store/core/create-store.ts
@@ -8,42 +8,47 @@ export function createStore<
   Services extends Record<string, any>,
   Params extends Record<string, any>
 >(options: CreateStoreOptions<State, Services, UseCases, Params>) {
-  return (services: Services, params: Params): Store<State, UseCases> => {
-    const listeners: ((state: State) => void)[] = []
-    let { state } = options
+  return {
+    providedOptions: options,
+    setupStore: (services: Services, params: Params): Store<State, UseCases> => {
+      const listeners: ((state: State) => void)[] = []
+      let { state } = options
 
-    const wrappedUseCases = objectKeys(options.useCases).reduce((useCases, key) => {
-      useCases[key as keyof typeof useCases] = ((...useCaseParams: Parameters<UseCases[typeof key]>) => {
-        state = produce(state, draftState => {
-          options.useCases[key](...useCaseParams)(draftState as State, services, params)
-        })
-        listeners.forEach(cb => cb(state))
-      }) as UseCases[keyof UseCases]
+      const wrappedUseCases = objectKeys(options.useCases).reduce((useCases, key) => {
+        useCases[key as keyof typeof useCases] = ((...useCaseParams: Parameters<UseCases[typeof key]>) => {
+          state = produce(state, draftState => {
+            options.useCases[key](...useCaseParams)(draftState as State, services, params)
+          })
+          listeners.forEach(cb => cb(state))
+        }) as UseCases[keyof UseCases]
 
-      return useCases
-    }, {} as UseCases)
+        return useCases
+      }, {} as UseCases)
 
-    return {
-      subscribe: cb => listeners.push(cb),
-      unsubscribe: cb => {
-        const index = listeners.findIndex(c => c === cb)
-        if (index !== -1) listeners.splice(index, 1)
-      },
-      useCases: wrappedUseCases,
-      init: () =>
-        options.onInit?.({
-          state,
-          services,
-          useCases: wrappedUseCases,
-          params,
-        }),
-      cleanUp: () =>
-        options.onCleanUp?.({
-          state,
-          services,
-          useCases: wrappedUseCases,
-          params,
-        }),
-    }
+      return {
+        subscribe: cb => listeners.push(cb),
+        unsubscribe: cb => {
+          const index = listeners.findIndex(c => c === cb)
+          if (index !== -1) listeners.splice(index, 1)
+        },
+        useCases: wrappedUseCases,
+        init: () =>
+          options.onInit?.({
+            state,
+            services,
+            useCases: wrappedUseCases,
+            params,
+          }),
+        cleanUp: () =>
+          options.onCleanUp?.({
+            state,
+            services,
+            useCases: wrappedUseCases,
+            params,
+          }),
+      }
+    },
   }
 }
+
+export const store = createStore
--- src/kit/context-store/react/react-adapter.tsx
+++ src/kit/context-store/react/react-adapter.tsx
@@ -1,31 +1,25 @@
-import { objectKeys } from 'kit/utils'
 import { Context, createContext, useContext, useEffect, useMemo, useState } from 'react'
-import { Store, createStore, UseCasesType } from '../core'
-import { ContextStore, CreateContextStoreOptions, ProviderProps, UseSelectorCallback } from './types'
+import { Store, UseCasesType, CreateStoreOptions } from '../core'
+import { ProviderProps, UseSelectorCallback } from './types'
 
 export function createContextStore<
   State extends Record<string, any>,
   Services extends Record<string, object>,
   UseCases extends UseCasesType,
   Params extends Record<string, any>
->(options: CreateContextStoreOptions<State, Services, UseCases, Params>): ContextStore<State, Services, UseCases, Params> {
-  let useCases: UseCases = objectKeys(options.useCases).reduce((uc, key) => {
-    uc[key] = (() => {}) as UseCases[keyof UseCases]
-    return uc
-  }, {} as UseCases)
+>(storeCreator: {
+  providedOptions: CreateStoreOptions<State, Services, UseCases, Params>
+  setupStore: (services: Services, params: Params) => Store<State, UseCases>
+}) {
   const Context = createContext<Pick<Store<State, UseCases>, 'subscribe' | 'unsubscribe'> & { getUseCases: () => UseCases }>({
     subscribe: () => {},
     unsubscribe: () => {},
-    getUseCases: () => useCases,
+    getUseCases: () => ({} as UseCases),
   })
-
   function Provider({ services, params, children }: ProviderProps<Services, Params>) {
-    const setupStore = useMemo(() => createStore<State, UseCases, Services, Params>(options), [])
     const store = useMemo(() => {
-      const store = setupStore(services, params)
-      useCases = store.useCases
-      return store
-    }, [services, setupStore, params])
+      return storeCreator.setupStore(services, params)
+    }, [services, params])
     const contextValue = useMemo(
       () => ({ subscribe: store.subscribe, unsubscribe: store.unsubscribe, getUseCases: () => store.useCases }),
       [store.subscribe, store.unsubscribe, store.useCases]
@@ -42,7 +36,7 @@ export function createContextStore<
     return <Context.Provider value={contextValue}>{children}</Context.Provider>
   }
 
-  return [Provider, buildUseSelector(options.state, Context), buildUseUseCases(Context)]
+  return [Provider, buildUseSelector(storeCreator.providedOptions.state, Context), buildUseUseCases(Context)]
 }
 
 function buildUseSelector<State extends Record<string, any>, UseCases extends UseCasesType>(
